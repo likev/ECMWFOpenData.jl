b71608cc51cad7b42b42558da86be195baba3cd3
likev <3146974+likev@users.noreply.github.com>
2024-04-18 03:13:32 +0000

    first version

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # ECMWFOpenData.jl
 A Julia package to download ECMWF open data
+
+```julia
+include("ECMWFOpenData.jl/ec-real-time-data.jl")
+
+using .ECRealTimeDatas
+```
+
+## Get Latest
+```julia
+    [latest()
+        latest(time=0)
+        latest(time=12)
+        latest(time=12, step=6)] .|> println
+```
+
+## Get Filename
+```julia
+basetime = latest().basetime
+steps = 0:24:72
+
+@. steps |> ( x->get_filename(basetime; x) ) |> println
+```
+
+## Download Files
+```julia
+# download latest
+download_steps(; steps = 0:24:72, path="ec-data")
+
+# download other basetime
+using Dates
+basetime=DateTime("2024-04-14 12", dateformat"yyyy-mm-dd HH")
+download_steps(basetime; steps = 0:24:72, path="ec-data")
+```
---
+++ ec-real-time-data.jl
@@ -0,0 +1,94 @@
+module ECRealTimeDatas
+
+export latest, get_url, get_filename, download_steps
+
+using Dates
+using Downloads
+
+function latest(; time=-1, step=0, model="ifs", stream="oper", type="fc", format="grib2", resol="0p25")
+    enddate = floor(now(), Hour(12))
+    if time == -1
+        delta = Hour(-12)
+    else
+        delta = Day(-1)
+        enddate += Hour(time) - Hour(enddate)
+    end
+
+    list = enddate:delta:enddate-Day(1)-Hour(6) |> collect
+    list .|> println
+
+    # @. list |> get_url |> (url->println("$url $(request(url).status)" ))
+    for basetime in list
+        url = get_url(basetime; step, model, stream, type, format, resol)
+        filename = get_filename(basetime; step, stream, type, format)
+        status = request(url).status
+
+        status == 200 && return (;url, basetime, filename)
+    end
+end
+
+#=
+Currently, ECMWF open real-time data are available from these locations:
+
+ECMWF, with ROOT set to https://data.ecmwf.int/forecasts
+Microsoft's Azure, with ROOT set to "https://ai4edataeuwest.blob.core.windows.net/ecmwf"
+Amazon's AWS, with ROOT set to "https://ecmwf-forecasts.s3.eu-central-1.amazonaws.com"
+
+https://confluence.ecmwf.int/display/DAC/ECMWF+open+data%3A+real-time+forecasts+from+IFS+and+AIFS
+=#
+
+function get_url(basetime; step=0, model="ifs", stream="oper", type="fc", format="grib2", resol="0p25")
+
+    ROOT = "https://ai4edataeuwest.blob.core.windows.net/ecmwf"
+
+    yyyymmdd = Dates.format(basetime, dateformat"yyyymmdd")
+    HH = Dates.format(basetime, dateformat"HH")
+
+    filename = get_filename(basetime; step, stream, type, format)
+
+    url = "$ROOT/$(yyyymmdd)/$(HH)z/$model/$resol/$stream/$filename"
+
+    url
+end
+
+function get_filename(basetime; step=0, stream="oper", type="fc", format="grib2")
+
+    yyyymmdd = Dates.format(basetime, dateformat"yyyymmdd")
+    HH = Dates.format(basetime, dateformat"HH")
+
+    filename = "$(yyyymmdd)$(HH)0000-$(step)h-$stream-$type.$format"
+
+    filename
+end
+
+function get_status_code(url)
+
+end
+
+function download_steps(basetime = latest().basetime; steps = 0:24:72, path="")
+    result = Dict{Int, String}()
+
+    mkpath(path)
+    for step in steps
+        url = get_url(basetime; step)
+        filename = get_filename(basetime; step)
+        
+        fullname = "$path$filename"
+        Downloads.download(url, fullname)
+
+        result[step] = fullname
+    end
+
+    result
+end
+
+function test()
+    [latest()
+        latest(time=0)
+        latest(time=12)
+        latest(time=12, step=6)] .|> println
+end
+
+# test()
+
+end
